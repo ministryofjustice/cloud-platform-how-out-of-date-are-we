bb0299a71d06f64e36bc273c09c4d18d5bf58488
David Salgado <david.salgado@digital.justice.gov.uk>
2020-02-10 17:36:09 +0000

    Customise the logic for red/amber/green
    * > 1 major version behind - red
    * No major, but > 5 minor versions behind - amber
    * No major, <= 5 minor versions behind - green

--- app.rb
+++ app.rb
@@ -11,6 +11,25 @@ end
 
 WHATUP_JSON_FILE = "data/helm-whatup.json"
 
+# Return success/warning/danger, depending on
+# how far behind latest the installed version
+# is.
+def version_lag_traffic_light(app)
+  installed = app.fetch("installedVersion").split(".")
+  latest = app.fetch("latestVersion").split(".")
+
+  major_diff = latest[0].to_i - installed[0].to_i
+  minor_diff = latest[1].to_i - installed[1].to_i
+
+  if major_diff > 1
+    "danger"
+  elsif minor_diff > 5
+    "warning"
+  else
+    "success"
+  end
+end
+
 ############################################################
 
 get "/" do
@@ -19,5 +38,6 @@ end
 
 get "/helm_whatup" do
   apps = JSON.parse(File.read WHATUP_JSON_FILE)
+  apps.map { |app| app["trafficLight"] = version_lag_traffic_light(app) }
   erb :helm_whatup, locals: { apps: apps }
 end
