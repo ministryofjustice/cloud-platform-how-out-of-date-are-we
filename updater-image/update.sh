1c960537a93424bcedb156bc56a4808caf6b695b
David Salgado <david@digitalronin.com>
2020-03-30 10:12:14 +0100

    Refactor the data update script
    This change makes the structure of the script
    clearer, so that a later change can add an update
    function for the terraform modules version data.

--- updater-image/update.sh
+++ updater-image/update.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
-set -euo pipefail
+main() {
+  set_api_key
+  helm_releases
+}
 
-aws s3 cp s3://${KUBECONFIG_S3_BUCKET}/${KUBECONFIG_S3_KEY} /tmp/kubeconfig
-kubectl config use-context ${KUBE_CLUSTER}
+set_api_key() {
+  aws s3 cp s3://${KUBECONFIG_S3_BUCKET}/${KUBECONFIG_S3_KEY} /tmp/kubeconfig
+  kubectl config use-context ${KUBE_CLUSTER}
+  export API_KEY=$(kubectl -n how-out-of-date-are-we get secrets how-out-of-date-are-we-api-key -o jsonpath='{.data.token}' | base64 -d)
+}
 
-helm repo update
 
-curl -H "X-API-KEY: $(kubectl -n how-out-of-date-are-we get secrets how-out-of-date-are-we-api-key -o jsonpath='{.data.token}' | base64 -d)" -d "$(helm whatup -o json)" ${HTTP_ENDPOINT}
+helm_releases() {
+  helm repo update
+  curl -H "X-API-KEY: ${API_KEY}" -d "$(helm whatup -o json)" ${HTTP_ENDPOINT}
+}
