70b5b8ef184bf11dff275090f10d1fea443700d0
David Salgado <david@digitalronin.com>
2020-04-24 15:47:18 +0100

    Post helm release data for live-1 & manager

--- updater-image/helm-releases.rb
+++ updater-image/helm-releases.rb
@@ -5,13 +5,29 @@
 require "json"
 require "open3"
 
-stdout, _, _ = Open3.capture3("helm whatup -o json")
-apps = JSON.parse(stdout)
+# This script expects to be invoked once the kube context for live-1 has been set up.
+
+def get_helm_release_data
+  stdout, _, _ = Open3.capture3("helm whatup -o json")
+  JSON.parse(stdout)
+end
+
+clusters = [
+  name: "live-1",
+  apps: get_helm_release_data,
+]
+
+# Switch to the manager cluster and repeat
+
+region = ENV.fetch("AWS_REGION")
+Open3.capture3("aws eks --region #{region} update-kubeconfig --name manager --alias manager")
+Open3.capture3("kubectl config use-context manager")
+
+clusters << { name: "manager", apps: get_helm_release_data }
+
 data = {
-  clusters: [
-    name: "live-1",
-    apps: apps,
-  ],
+  clusters: clusters,
   updated_at: Time.now
 }
+
 puts data.to_json
