630654d86043612bad37c8aba3f00015ca8f7443
David Salgado <david@digitalronin.com>
2020-06-22 16:00:40 +0100

    Use real data for the dashboard page
    There is some code duplication here, applying the
    same logic for the dashboard page as for the
    individual HOODAW pages. This isn't great, but we
    can live with it for now, and fix it later.

--- app.rb
+++ app.rb
@@ -29,6 +29,39 @@ def require_api_key(request)
   end
 end
 
+def dashboard_data
+  # TODO: there is a lot of duplication here from the code for the individual docpaths. Fix that.
+  updated = []
+
+  terraform_modules, updated_at = get_list_and_updated_at(datafile("terraform_modules"), "out_of_date_modules")
+  updated << updated_at
+
+  documentation_pages, updated_at = get_list_and_updated_at(datafile("documentation"), "pages")
+  updated << updated_at
+
+  repositories, updated_at = get_list_and_updated_at(datafile("repositories"), "repositories")
+  repositories.reject! { |repo| repo["status"] == "PASS" }
+  updated << updated_at
+
+  clusters, updated_at = get_list_and_updated_at(datafile("helm_whatup"), "clusters")
+  out_of_date_apps = clusters.map { |cluster| cluster.fetch("apps") }.flatten
+    .filter { |app| version_lag_traffic_light(app) == "danger" }
+  updated << updated_at
+
+  {
+    updated_at: updated.sort.first,
+    data: {
+      action_items: {
+        helm_whatup: out_of_date_apps.length,
+        terraform_modules: terraform_modules.length,
+        documentation: documentation_pages.length,
+        repositories: repositories.length,
+      },
+      action_required: true
+    }
+  }
+end
+
 # key is the name of the key in our datafile which contains the list of
 # elements we're interested in.
 def fetch_data(docpath, key)
@@ -75,19 +108,9 @@ get "/" do
 end
 
 get "/dashboard" do
-  locals = {
+  locals = dashboard_data.merge(
     active_nav: "/dashboard",
-    updated_at: nil,
-    data: {
-      action_items: {
-        helm_whatup: 1,
-        terraform_modules: 2,
-        documentation: 0,
-        repositories: 3
-      },
-      action_required: true
-    }
-  }
+  )
   erb :dashboard, locals: locals
 end
 
