91fb10b84b4c33d74e589141d87d9928c9f84203
David Salgado <david@digitalronin.com>
2020-09-11 14:47:57 +0100

    Use oldest json file mtime as "updated_at"

--- lib/namespace_costs.rb
+++ lib/namespace_costs.rb
@@ -6,11 +6,22 @@ class NamespaceCosts
   end
 
   def updated_at
-    Time.now # TODO: oldest json file modification date
+    list # sets @updated_at as a side-effect
+    @updated_at
+  end
+
+  def set_updated_at(file)
+    @updated_at ||= Time.now
+    @updated_at = [
+      @updated_at,
+      File.stat("data/namespace/costs/cccd-dev.json").mtime
+    ].min
   end
 
   def list
-    Dir["#{dir}/*.json"].sort.map { |file| process(file) }
+    @list ||= Dir["#{dir}/*.json"]
+      .sort
+      .map { |file| process(file) }
   end
 
   private
@@ -18,6 +29,7 @@ class NamespaceCosts
   # file contains the output of `infracost --tfdir .` for each namespace
   def process(file)
     items = JSON.parse(File.read(file))
+    set_updated_at(file)
     total = items.map { |item| item["monthlyCost"].to_f }.sum
     namespace = File.basename(file).sub(".json", "")
     {
