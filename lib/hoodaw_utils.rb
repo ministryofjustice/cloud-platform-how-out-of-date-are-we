b55b3471bf99b70e25fd94c300dd1a379f2cd27b
David Salgado <david@digitalronin.com>
2020-09-11 14:40:55 +0100

    Add 'commify' utility func, 1234,5678 -> 1,234.57

--- lib/hoodaw_utils.rb
+++ lib/hoodaw_utils.rb
@@ -3,6 +3,16 @@ module HoodawUtils
   def snake_case_to_capitalised(str)
     str.split("_").map(&:capitalize).join(" ")
   end
+
+  def decimals(value, decimals = 2)
+    sprintf("%0.#{decimals}f", value)
+  end
+
+  def commify(value)
+    whole, decimal = decimals(value).split(".")
+    with_commas = whole.gsub(/\B(?=(...)*\b)/, ",")
+    [with_commas, decimal].join(".")
+  end
 end
 
 helpers HoodawUtils
