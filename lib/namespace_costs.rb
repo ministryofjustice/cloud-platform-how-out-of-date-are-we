5c26203e888942c08a1fad981906c07ca7636a48
David Salgado <david@digitalronin.com>
2020-09-11 14:41:19 +0100

    Show monthly cost per namespace

--- lib/namespace_costs.rb
+++ lib/namespace_costs.rb
@@ -10,6 +10,19 @@ class NamespaceCosts
   end
 
   def list
-    []
+    Dir["#{dir}/*.json"].sort.map { |file| process(file) }
+  end
+
+  private
+
+  # file contains the output of `infracost --tfdir .` for each namespace
+  def process(file)
+    items = JSON.parse(File.read(file))
+    total = items.map { |item| item["monthlyCost"].to_f }.sum
+    namespace = File.basename(file).sub(".json", "")
+    {
+      name: namespace,
+      total: total,
+    }
   end
 end
