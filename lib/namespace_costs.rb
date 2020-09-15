a069aeb10109d85efc065382575efe95a6092119
David Salgado <david@digitalronin.com>
2020-09-15 17:15:00 +0100

    Add caching of the namespace costs list
    Generating the list requires hundreds of lookups,
    which is quite slow on dynamodb, so cache the list
    for an hour.

--- lib/namespace_costs.rb
+++ lib/namespace_costs.rb
@@ -2,6 +2,8 @@ class NamespaceCost
   def initialize(params)
     @store = params.fetch(:store)
     @file = params.fetch(:file)
+    @data = params[:data] # used when recreating from cached JSON data
+    @updated_at = params[:updated_at] # used when recreating from cached JSON data
   end
 
   def data
@@ -13,7 +15,7 @@ class NamespaceCost
   end
 
   def updated_at
-    @store.stored_at(@file)
+    @updated_at ||= @store.stored_at(@file)
   end
 
   def resources
@@ -31,10 +33,23 @@ class NamespaceCost
   def total
     @total ||= data.map { |item| item["monthlyCost"].to_f }.sum
   end
+
+  def to_json(_)
+    {
+      file: @file,
+      data: data,
+      updated_at: updated_at,
+    }.to_json
+  end
 end
 
 class NamespaceCosts
   attr_reader :dir, :store
+
+  # Generating the list requires a lot of file lookups, which is slow if we're using
+  # dynamodb, so we cache the generated list in this file.
+  CACHE_FILENAME = "data/namespace_costs.json"
+  CACHE_SECONDS = 3600
 
   def initialize(params)
     @store = params.fetch(:store)
@@ -56,6 +71,49 @@ class NamespaceCosts
   private
 
   def costs_list
+    if cache_expired?
+      data = {
+        "list" => build_costs_list,
+        "updated_at" => Time.now,
+      }
+      cache(data)
+    else
+      data = cached_costs_list
+    end
+    data["list"]
+  end
+
+  def cache_expired?
+    if store.exists?(CACHE_FILENAME)
+      (store.stored_at(CACHE_FILENAME) + CACHE_SECONDS) < Time.now
+    else
+      true
+    end
+  end
+
+  def cached_costs_list
+    data = JSON.parse(store.retrieve_file CACHE_FILENAME)
+
+    list = data["list"].map { |d|
+      NamespaceCost.new(
+        file: d["file"],
+        data: d["data"],
+        updated_at: d["updated_at"],
+        store: nil,
+      )
+    }
+
+    {
+      "list" => list,
+      "updated_at" => data["updated_at"],
+    }
+  end
+
+  def cache(data)
+    store.store_file(CACHE_FILENAME, data.to_json)
+  end
+
+  def build_costs_list
     store.list_files
       .filter { |f| f =~ %r[^#{dir}/.*.json$] }
       .map { |file| NamespaceCost.new(file: file, store: store) }
