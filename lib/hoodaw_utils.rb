15ef9d7cd702896656ee47638760ab741bbb96a1
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2020-09-22 17:43:36 +0100

    Add github team url to team_name and hyperlink it

--- lib/hoodaw_utils.rb
+++ lib/hoodaw_utils.rb
@@ -1,4 +1,5 @@
 module HoodawUtils
+  GITHUB_TEAM_URL = "https://github.com/orgs/ministryofjustice/teams/"
   # "foo_bar_baz" => "Foo Bar Baz"
   def snake_case_to_capitalised(str)
     str.split("_").map(&:capitalize).join(" ")
@@ -12,6 +13,10 @@ module HoodawUtils
     whole, decimal = decimals(value).split(".")
     with_commas = whole.gsub(/\B(?=(...)*\b)/, ",")
     [with_commas, decimal].join(".")
+  end
+
+  def github_team_url(team_name)
+    link_to(team_name.to_s,GITHUB_TEAM_URL+team_name.to_s)
   end
 
   def link_each_to(domain_names)
