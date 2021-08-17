5a85f7d4e6d0bae916c64bcd912907b8d2d7b8a3
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2021-08-17 15:37:55 +0100

    Refactor codebase so main package is just performing execution
    Authentication will eventually move to an external package, meaning I
    will need to import it in main.

--- reports/ingress-weighting/main.go
+++ reports/ingress-weighting/main.go
@@ -4,12 +4,14 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
 	"flag"
 	"log"
 	"net/http"
 	"os"
 	"time"
 
+	// "github.com/ministryofjustice/cloud-platform-how-out-of-date-are-we/reports/pkg/authenticate"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
@@ -20,32 +22,56 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 )
 
+type resourceMap map[string]interface{}
+
 var (
 	hoodawApiKey   = flag.String("hoodawAPIKey", os.Getenv("HOODAW_API_KEY"), "API key to post data to the 'How out of date are we' API")
 	hoodawHost     = flag.String("hoodawHost", os.Getenv("HOODAW_HOST"), "Hostname of the 'How out of date are we' API")
 	bucket         = flag.String("bucket", os.Getenv("KUBECONFIG_S3_BUCKET"), "AWS S3 bucket for kubeconfig")
 	configFile     = flag.String("configFile", os.Getenv("KUBECONFIG_S3_KEY"), "Name of kubeconfig file in S3 bucket")
-	hoodawEndpoint = "/ingress_weighting"
+	annotation     = flag.String("annotation", "external-dns.alpha.kubernetes.io/aws-weight", "String of the annotation to check")
+	hoodawEndpoint = flag.String("hoodawEndpoint", "/ingress_weighting", "Endpoint to send the data to")
 )
 
 func main() {
 	flag.Parse()
 
+	// Authenticate to a Kubernetes cluster
+	// clientset, err := authenticate.FromS3Bucket(*bucket, *configFile)
+	clientset, err := FromS3Bucket(*bucket, *configFile)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	// Grab all ingress resources without required annotation
+	jsonToPost, err := IngressWithoutAnnotation(clientset)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	// Post json to hoowdaw api
+	err = postToApi(jsonToPost)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+}
+
+func FromS3Bucket(bucket, configFile string) (clientset *kubernetes.Clientset, err error) {
 	buff := &aws.WriteAtBuffer{}
 	downloader := s3manager.NewDownloader(session.New(&aws.Config{
 		Region: aws.String("eu-west-2"),
 	}))
 
 	numBytes, err := downloader.Download(buff, &s3.GetObjectInput{
-		Bucket: aws.String(*bucket),
-		Key:    aws.String(*configFile),
+		Bucket: aws.String(bucket),
+		Key:    aws.String(configFile),
 	})
 
 	if err != nil {
-		log.Fatalln(err.Error())
+		return nil, err
 	}
 	if numBytes < 1 {
-		log.Fatalln("The file downloaded is incorrect.")
+		return nil, errors.New("The file downloaded is incorrect.")
 	}
 
 	data := buff.Bytes()
@@ -53,54 +79,27 @@ func main() {
 	// use the current context in kubeconfig
 	config, err := clientcmd.NewClientConfigFromBytes(data)
 	if err != nil {
-		log.Println(err.Error())
+		return nil, err
 	}
 
 	clientConfig, err := config.ClientConfig()
 	if err != nil {
-		log.Fatalln(err.Error())
+		return nil, err
 	}
 
 	// create the clientset
-	clientset, err := kubernetes.NewForConfig(clientConfig)
+	clientset, err = kubernetes.NewForConfig(clientConfig)
 	if err != nil {
-		log.Println(err.Error())
+		return nil, err
 	}
 
-	ingress, err := clientset.NetworkingV1beta1().Ingresses("").List(context.TODO(), metav1.ListOptions{})
+	return
+}
+
+func postToApi(jsonToPost []byte) error {
+	req, err := http.NewRequest("POST", *hoodawHost+*hoodawEndpoint, bytes.NewBuffer(jsonToPost))
 	if err != nil {
-		log.Println(err.Error())
-	}
-
-	// s contains a slice of maps, each map will be iterated over when placed in a dashboard.
-	s := make([]map[string]string, 0)
-
-	// For each ingress resource, check
-	for _, i := range ingress.Items {
-		if _, ok := i.Annotations["external-dns.alpha.kubernetes.io/aws-weight"]; !ok {
-			for _, v := range i.Spec.TLS {
-				m := make(map[string]string)
-				m["namespace"] = i.GetName()
-				m["hostname"] = v.Hosts[0]
-				s = append(s, m)
-			}
-		}
-	}
-
-	type genericMap map[string]interface{}
-	postToJson := genericMap{
-		"updated_at":        time.Now().Format("2006-01-2 15:4:5 UTC"),
-		"weighting_ingress": s,
-	}
-
-	jsonStr, err := json.Marshal(postToJson)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	req, err := http.NewRequest("POST", *hoodawHost+hoodawEndpoint, bytes.NewBuffer(jsonStr))
-	if err != nil {
-		log.Fatalln(err)
+		return err
 	}
 
 	req.Header.Add("X-API-KEY", *hoodawApiKey)
@@ -110,8 +109,44 @@ func main() {
 
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Fatalln("Error on POST response. \n", err)
+		return err
 	}
 
 	defer resp.Body.Close()
+
+	return nil
+}
+
+func IngressWithoutAnnotation(clientset *kubernetes.Clientset) ([]byte, error) {
+	ingress, err := clientset.NetworkingV1beta1().Ingresses("").List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	// s contains a slice of maps, each map will be iterated over when placed in a dashboard.
+	s := make([]map[string]string, 0)
+
+	// For each ingress resource, check
+	for _, i := range ingress.Items {
+		if _, ok := i.Annotations[*annotation]; !ok {
+			for _, v := range i.Spec.TLS {
+				m := make(map[string]string)
+				m["namespace"] = i.GetName()
+				m["hostname"] = v.Hosts[0]
+				s = append(s, m)
+			}
+		}
+	}
+
+	postToJson := resourceMap{
+		"updated_at":        time.Now().Format("2006-01-2 15:4:5 UTC"),
+		"weighting_ingress": s,
+	}
+
+	jsonStr, err := json.Marshal(postToJson)
+	if err != nil {
+		return nil, err
+	}
+
+	return jsonStr, nil
 }
