2256c5f2f7c55a3c0714627652f37c925e81db39
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2021-08-26 12:08:53 +0100

    Create types package to avoid deduplication

---
+++ reports/pkg/types/go.mod
@@ -0,0 +1,3 @@
+module github.com/ministryofjustice/cloud-platform-how-out-of-date-are-we/reports/pkg/types
+
+go 1.16
---
+++ reports/pkg/types/types.go
@@ -0,0 +1,9 @@
+// Package types provides all shared custom data types used in the HOODAW application.
+package types
+
+// resourceMap is used to store both string:string and string:map[string]string key
+// value pairs. The HOODAW API requires the first entry of map to contain a string:string,
+// the rest of the map consists of a primary key (string) with a value containing a map (string:string)
+// of Kubernetes namespaces and hostnames that don't contain the annotation contained within
+// the variable 'annotation'.
+type ResourceMap map[string]interface{}
