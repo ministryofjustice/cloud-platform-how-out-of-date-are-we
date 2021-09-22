10f6320538787013475974e6904e7efbbe77a0f3
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2021-09-22 15:20:07 +0100

    Add QueryApi function and document exported package
    I've found myself writing the same block of code to query the hoodaw
    API. This commit creates a function to make any future code pragmatic.

---
+++ reports/pkg/hoodaw/document.go
@@ -0,0 +1,4 @@
+/*
+Package hoodaw implements operators to interact with the How-out-of-date-are-we API.
+*/
+package hoodaw //import "github.com/ministryofjustice/cloud-platform-how-out-of-date-are-we/reports/pkg/hoodaw"
