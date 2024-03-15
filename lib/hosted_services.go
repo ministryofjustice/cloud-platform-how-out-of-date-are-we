034a76a25b18b414f8377eba401792190d3a31e5
jackstockley89 <jstock8904@gmail.com>
2024-03-15 10:37:15 +0000

    feat(lib): Create page template

--- lib/hosted_services.go
+++ lib/hosted_services.go
@@ -9,6 +9,19 @@ import (
 	"github.com/ministryofjustice/cloud-platform-how-out-of-date-are-we/utils"
 )
 
+type HostedService struct {
+	Namespace    string `json:"namespace"`
+	Application  string `json:"application"`
+	BusinessUnit string `json:"business_unit"`
+	TeamName     string `json:"team_name"`
+	SlackChannel string `json:"slack_channel"`
+	SourcesCode  string `json:"source_code"`
+	DomainName   string `json:"domain_name"`
+
+	TotalNS,
+	TotalApps int
+}
+
 func HostedServices(w http.ResponseWriter, r *http.Request) {
 	t := template.Must(template.ParseFiles("lib/templates/hosted_services.html"))
 
@@ -19,10 +32,16 @@ func HostedServices(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// parse json data
-	var services []utils.HostedService
+	var services []HostedService
 	err = json.Unmarshal(data, &services)
 	if err != nil {
 		fmt.Println(err)
+	}
+
+	// count the number of namespaces and applications in the json data and add to the struct
+	for i, _ := range services {
+		services[i].TotalNS = len(services[i].Namespace)
+		services[i].TotalApps = len(services[i].Application)
 	}
 
 	// render template
